1c47b5f9a4fcb108e18d1ac8e2c960a5b66ba4fa
Aaron Leung <the.other.aaron@sourcegraph.com>
2017-02-16 17:50:06 -0800

    add more annotation stuff

--- src/main/java/com/sourcegraph/Shiny.java
+++ src/main/java/com/sourcegraph/Shiny.java
@@ -1,5 +1,11 @@
 package com.sourcegraph;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@SuppressWarnings("HEY")
+@Retention(RetentionPolicy.CLASS)
+@Shiny(isSparkly = true)
 public @interface Shiny {
 
     boolean isSparkly() default false;
