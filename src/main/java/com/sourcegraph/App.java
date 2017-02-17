c2342e667ee22dbedb73f188e3524462da74a94f
Aaron Leung <the.other.aaron@sourcegraph.com>
2017-02-16 16:48:16 -0800

    remove broken code

--- src/main/java/com/sourcegraph/App.java
+++ src/main/java/com/sourcegraph/App.java
@@ -44,7 +44,6 @@ public class App {
     @JsonInclude
     public void whatever() {
         new Cup<String>("some string") {}.refill("some more string");
-        Broken.foo();
     }
 
     @Shiny(isSparkly = true)
